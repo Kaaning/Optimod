f1fb263eff1c0f952d40bdbf7f609067c2218549
WVitali <ebill@hotmail.fr>
2014-11-04 10:59:02 +0100

    Base de Command

---
+++ src/controleur/Command.java
@@ -0,0 +1,14 @@
+package controleur;
+
+public class Command {
+
+	public int execute(){
+		return 0;
+	}
+	
+	public int unexecute(){
+		return 0;
+	}
+	
+	
+}
---
+++ src/controleur/Invoker.java
@@ -0,0 +1,17 @@
+package controleur;
+
+import java.util.ArrayList;
+
+public class Invoker {
+	
+	private ArrayList<Command> commands;
+	private Command currentCommand;
+	
+	public int undo(){
+		return 0;
+	}
+	
+	public int redo(){
+		return 0;
+	}
+}
