83e246a4c8654c8d703d8915e906cffc5666fb99
WVitali <ebill@hotmail.fr>
2014-11-04 11:57:01 +0100

    Mise en place

---
+++ src/modele/Noeud.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Noeud {
+	
+	private int id;
+	private int x;
+	private int y;
+	
+	Noeud(int id, int x, int y){
+		this.id=id;
+		this.x=x;
+		this.y=y;
+	}
+
+}
---
+++ src/modele/Tronçon.java
@@ -0,0 +1,15 @@
+package modele;
+
+public class Tronçon {
+	
+	private String nomRue;
+	private float vitesse;
+	private float longueur;
+	
+	public Tronçon(String nomRue, float vitesse, float longueur){
+		this.nomRue=nomRue;
+		this.vitesse=vitesse;
+		this.longueur=longueur;
+	}
+
+}
