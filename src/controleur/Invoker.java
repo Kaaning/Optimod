bf886b38387d46101fd7aef357c2536d3f60ff59
A. El Ouassouli <amine.elouassouli@gmail.com>
2014-11-10 21:24:41 +0100

    Mise en oeuvre structure Command

--- src/controleur/Invoker.java
+++ src/controleur/Invoker.java
@@ -5,13 +5,50 @@ import java.util.ArrayList;
 public class Invoker {
 	
 	private ArrayList<Command> commands;
-	private Command currentCommand;
+	private int currentCommandIndex;
 	
+	/**
+	 * Defait la dernière commande faite .
+	 * @return 0 si l'oppration réussi ou 1 s'il n'y a pas de command à desexecuter
+	 */
 	public int undo(){
-		return 0;
+		if (currentCommandIndex != -1) {
+			this.commands.get(currentCommandIndex).unexecute();
+			this.currentCommandIndex --;
+			return 0;
+		}
+		else {
+			return 1;
+		}
 	}
 	
+	/**
+	 * Refait la dernière commande defaite .
+	 * @return 0 si l'oppration réussi ou 1 s'il n'y a pas de command à desexecuter
+	 */
 	public int redo(){
+		if ( this.currentCommandIndex < this.commands.size() -1 ) {
+			this.currentCommandIndex ++;
+			this.commands.get(currentCommandIndex).execute();
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+	
+	/**
+	 * Refait la dernière commande defaite .
+	 * @return 0 si l'oppration réussi ou 1 s'il n'y a pas de command à desexecuter
+	 */
+	
+	public int addCommand(Command command) {
+		if (this.currentCommandIndex == this.commands.size() -1 ) {
+			this.commands = new ArrayList<Command>(this.commands.subList(0, this.currentCommandIndex));
+		}
+		this.commands.add(command);
+		currentCommandIndex ++;
+		
 		return 0;
 	}
 }
